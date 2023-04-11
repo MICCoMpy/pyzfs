e2831033173be77ed568479fef2b80026f077e13
Victor Yu <victor.wz.yu.1012@gmail.com>
2023-04-11 14:58:25 -0500

    Clean up Python 2

--- pyzfs/common/ft.py
+++ pyzfs/common/ft.py
@@ -1,4 +1,3 @@
-from __future__ import absolute_import, division, print_function
 from mpi4py import MPI
 try:
     from cupy.fft import fftn, ifftn, rfftn, irfftn, fftshift, ifftshift
--- pyzfs/common/parallel.py
+++ pyzfs/common/parallel.py
@@ -1,4 +1,3 @@
-from __future__ import absolute_import, division, print_function
 from time import sleep
 import numpy as np
 from mpi4py import MPI
